b68345172d2f6f2b955c89d5f5fa6c95aa700a99
ViktorUserName <vityachishevich@gmail.com>
2023-01-03 19:43:25 +0300

    add credentials

--- back/service/server.js
+++ back/service/server.js
@@ -1,5 +1,7 @@
 import express from "express";
 import cors from 'cors';
+import fs from 'fs';
+import admin from 'firebase-admin'
 import { MongoClient } from "mongodb";
 const app = express();
 let corsOptions = {
@@ -9,26 +11,54 @@ let corsOptions = {
 app.use(express.json())
 app.use(cors(corsOptions));
 
+const credentions = JSON.parse(
+    fs.readFileSync('./credentions.json')
+)
+admin.initializeApp({
+    credential: admin.credential.cert(credentions)
+})
+
+app.use(async (request, response, next) => {
+    const { authtoken } = request.headers;
+    if(authtoken) {
+        try {
+            const user = await admin.auth().verifyIdToken(authtoken);
+            request.user = user;
+        } catch (e) {
+            response.sendStatus(400);
+        }
+    }
+    next();
+})
+
 app.get('/api/articles/:name', async function(request, response){
     const { name } = request.params;
+    // const { uid } = request.user; ////
 
     const client = new MongoClient('mongodb://0.0.0.0:27017/')
     await client.connect();
     const db = client.db('react-blog-db');
     const article = await db.collection('articles').findOne({name})
     if(article){
+        // const upvoteIds = article.upvoteIds || []; /////
+        // article.canUpvote = uid && !upvoteIds.unclude(uid) ////
         response.json(article)
     }else {
         response.send('bad response')
     }
 })
 
-
+app.use ((request, response, next) => {
+    if(request.user){
+        next();
+    } else { response.sendStatus(401)}
+})
 
 app.put('/api/articles/:name/upvote', async function(request, response){
     const { name } = request.params;
     const client = new MongoClient('mongodb://0.0.0.0:27017/');
     await client.connect();
+    
 
     const db = client.db('react-blog-db');
     await db.collection('articles').updateOne({name}, {
--- front/src/pages/CreateLogin.js
+++ front/src/pages/CreateLogin.js
@@ -1,10 +1,55 @@
 import React from 'react';
+import {useState} from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth';
 
 const CreateLogin = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('')
+    const [error, setError] = useState('');
+
+    const navigate = useNavigate()
+
+    const createAccount = async () => {
+        try {
+            if(password !== confirmPassword) {
+                setError ('Password dostn confitm')
+                return
+            }
+            await createUserWithEmailAndPassword(getAuth(), email, password )
+            navigate('/articles')
+        } catch (e) {
+            setError(e.message);
+        }
+    }
+    
+
+
     return (
-       <h1>
-        Create Acount
-       </h1>
+       <>
+            <h1>
+            Create
+            </h1>
+            {error && <p>{error}</p>}
+            <input 
+                placeholder='your email adress'
+                value={email}
+                onChange={e => setEmail(e.target.value)}
+             />
+            <input type="password" 
+                placeholder='your password'
+                value={password}
+                onChange={e => setPassword(e.target.value)}
+            />
+            <input type="password" 
+                placeholder='Re-enter your password'
+                value={confirmPassword}
+                onChange={e => setConfirmPassword(e.target.value)}
+            />
+            <button onClick={createAccount}>Create Acc</button>
+            <Link to='/login'> Alredy have?</Link>
+       </>
     );
 };
 
